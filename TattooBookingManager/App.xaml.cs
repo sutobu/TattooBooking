f08343947d66a1d110fcd8cfdd271c4b959a19e2
Marina <venskaanika@gmail.com>
2025-06-09 23:34:42 +0300

    ap

--- TattooBookingManager/App.xaml.cs
+++ TattooBookingManager/App.xaml.cs
@@ -9,6 +9,15 @@ namespace TattooBookingManager
     /// </summary>
     public partial class App : Application
     {
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            base.OnStartup(e);
+            DispatcherUnhandledException += (s, args) =>
+            {
+                MessageBox.Show($"An error occurred: {args.Exception.Message}", "Error", MessageBoxButton.OK, MessageBoxImage.Error);
+                args.Handled = true;
+            };
+        }
     }
 
 }
